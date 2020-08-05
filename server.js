0ffee792a4b74d83add28258240a997a39e90a56
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-05 14:10:08 -0700

    refactored party routing

--- server.js
+++ server.js
@@ -9,49 +9,6 @@ const inputCheck = require('./utils/inputCheck')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// GET all parties as json
-app.get('/api/parties', (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.all(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-// GET single party by id as json
-app.get('/api/party/:id', (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.get(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: row
-    });
-  });
-});
-app.delete('/api/party/:id', (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      return;
-    }
-
-    res.json({ message: 'successfully deleted', changes: this.changes });
-  });
-});   
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
